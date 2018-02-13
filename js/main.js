4f4556b398760f25ae372d7639ad5f3e3542271c
krista triviso <krista.triviso91@gmail.com>
2018-02-13 12:40:54 -0500

    javascript game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
-console.log("Up and running!");
+// console.log("Up and running!");
+//
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+//
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+// console.log("User flipped " + cardThree);
+// console.log("User flipped " + cardFour);
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
