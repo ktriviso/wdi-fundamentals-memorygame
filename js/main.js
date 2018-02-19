4c548e5a83e3088bc173ca698f7566ac715374a0
krista triviso <krista.triviso91@gmail.com>
2018-02-18 20:06:13 -0500

    full game structure

--- js/main.js
+++ js/main.js
@@ -1,42 +1,72 @@
-var cards = [
-    {
-        rank: "queen",
-        suit: "hearts",
+const cards = [{
+        rank: 'queen',
+        suit: 'hearts',
         cardImage: "images/queen-of-hearts.png"
+
     },
     {
-        rank: "queen",
-        suit: "diamonds",
+        rank: 'queen',
+        suit: 'diamonds',
         cardImage: "images/queen-of-diamonds.png"
+
     },
     {
-        rank: "king",
-        suit: "hearts",
+        rank: 'king',
+        suit: 'hearts',
         cardImage: "images/king-of-hearts.png"
+
     },
     {
-        rank: "king",
-        suit: "diamonds",
+        rank: 'king',
+        suit: 'diamonds',
         cardImage: "images/king-of-diamonds.png"
-    }
-];
-var cardsInPlay = [];
 
-function checkForMatch(){
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+    }
+]
+
+const cardsInPlay = []
+const checkForMatch = () => {
+    let cardOneInPlay = cardsInPlay[0]
+    let cardTwoInPlay = cardsInPlay[1]
+
+    if (cardOneInPlay === cardTwoInPlay) {
+        alert("Its a match!")
     } else {
-        console.log("Sorry, try again.");
+        alert("Its not a match!")
+    }
+}
+const flipCard = (e) => {
+    const cardOnTheDom = e.target
+    const cardId = cardOnTheDom.getAttribute('data-id')
+    const card = cards[cardId]
+
+    cardOnTheDom.setAttribute('src', card.cardImage)
+    cardsInPlay.push(card)
+    if (cardsInPlay.length === 2) {
+        checkForMatch()
     }
 }
 
-function flipCard(cardId){
-    console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].suit);
-    console.log(cards[cardId].cardImage);
-    cardsInPlay.push(cards[cardId].rank);
-    checkForMatch();
+const createBoard = () => {
+    const gameBoard = document.getElementById('game-board')
+
+    for (let i = 0; i < cards.length; i++) {
+        let cardElement = document.createElement('img')
+        cardElement.src = 'images/back.png'
+        cardElement.setAttribute('data-id', i)
+        let dataId = cardElement.getAttribute('data-id')
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement)
+    }
+}
+createBoard()
+
+const reset = (e) => {
+    window.location.reload()
 }
 
-flipCard(0);
-flipCard(2);
+const resetClick = () => {
+    const resetButton = document.getElementById('reset-button')
+    resetButton.addEventListener('click', reset)
+}
+resetClick()
